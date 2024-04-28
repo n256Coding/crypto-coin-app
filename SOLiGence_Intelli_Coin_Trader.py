d918e2f1a7e018fc49d991d3727e614f6f28ddda
Nishan <nishan256@gmail.com>
2024-04-28 17:16:21 +0100

    trying to create subpages (does not work yet)

--- SOLiGence_Intelli_Coin_Trader.py
+++ SOLiGence_Intelli_Coin_Trader.py
@@ -1,41 +1,85 @@
-import streamlit as st
+# import streamlit as st
+from st_pages import Page, Section, show_pages, add_page_title
+
+# st.set_page_config(page_title="Intelli Coin Trader", page_icon="📊")
 
 
-st.set_page_config(page_title="Intelli Coin Trader", page_icon="📊")
 
-def intro():
-    import streamlit as st
+show_pages(
+    [
+        Page("pages/welcome.py", "Home", "🏠"),
+        Page("pages/EDA.py", "EDA", "📈"),
+        Page("pages/Model_Evaluations.py", "Model Evaluations", "📈"),
+        # Page("other_pag/page2.py", "Page 2", ":books:"),
+        Section("Forecaster", icon="🎈️"),
+        Page("pages/Moving_Average.py", "Moving Average", "📈"),
+        Page("pages/Trade_Assistant.py", "Trade Assistant", "📈"),
+        # Pages after a section will be indented
+        # Page("Another page", icon="💪"),
+        # Unless you explicitly say in_section=False
+        # Page("Not in a section", in_section=False)
+    ]
+)
 
-    st.write("# Welcome to Streamlit! 👋")
-    st.sidebar.success("Select a demo above.")
+add_page_title()
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
 
-        **👈 Select a demo from the dropdown on the left** to see some examples
-        of what Streamlit can do!
+# with st.echo("below"):
+#     from st_pages import Page, Section, add_page_title, show_pages
 
-        ### Want to learn more?
+#     "## Declaring the pages in your app:"
 
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
+#     show_pages(
+#       [
+#         Page("pages/welcome.py", "Home", "🏠"),
+#         Page("pages/2_📈_EDA.py", "EDA", "📈"),
+#         Page("pages/3_📈_Model_Evaluations.py", "Model Evaluations", "📈"),
+#         # Page("other_pages/page2.py", "Page 2", ":books:"),
+#         Section("Forecast", icon="🎈️"),
+#         Page("pages/4_📈_Moving_Average.py", "Moving Average", "📈", in_section=True),
+#         Page("pages/5_📈_Trade_Assistant.py", "Trade Assistant", "📈", in_section=True),
+#         # Pages after a section will be indented
+#         # Page("Another page", icon="💪"),
+#         # Unless you explicitly say in_section=False
+#         # Page("Not in a section", in_section=False)
+#       ]
+#     )
 
-        ### See more complex demos
+#     add_page_title()  # Optional method to add title and icon to current page
 
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# def intro():
+#     import streamlit as st
 
-page_names_to_funcs = {
-    "—": intro,
-    # "Mapping Demo": mapping_demo
-}
+#     st.write("# Welcome to Streamlit! 👋")
+#     st.sidebar.success("Select a demo above.")
+
+#     st.markdown(
+#         """
+#         Streamlit is an open-source app framework built specifically for
+#         Machine Learning and Data Science projects.
+
+#         **👈 Select a demo from the dropdown on the left** to see some examples
+#         of what Streamlit can do!
+
+#         ### Want to learn more?
+
+#         - Check out [streamlit.io](https://streamlit.io)
+#         - Jump into our [documentation](https://docs.streamlit.io)
+#         - Ask a question in our [community
+#           forums](https://discuss.streamlit.io)
+
+#         ### See more complex demos
+
+#         - Use a neural net to [analyze the Udacity Self-driving Car Image
+#           Dataset](https://github.com/streamlit/demo-self-driving)
+#         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+#     """
+#     )
+
+# page_names_to_funcs = {
+#     "—": intro,
+#     # "Mapping Demo": mapping_demo
+# }
 
 # demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
 # demo_name = st.sidebar()
---
+++ pages/welcome.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Intelli Coin Trader", page_icon="📊")
+
+st.write("# Welcome to Streamlit! 👋")
+#     st.sidebar.success("Select a demo above.")
