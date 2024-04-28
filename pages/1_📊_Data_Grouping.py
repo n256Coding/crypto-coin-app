5c8e84a3d210388ef3d5705e52a4f482ba90c9e4
Nishan <nishan256@gmail.com>
2024-04-28 17:08:52 +0100

    improved cluster allocation visualization

--- pages/1_📊_Data_Grouping.py
+++ pages/1_📊_Data_Grouping.py
@@ -8,17 +8,20 @@ from services.main_service import perform_clusterization
 st.set_page_config(page_title="Data Grouping", page_icon="📊")
 
 st.markdown("# Data Grouping")
-# st.sidebar.header("Cluster Distribution")
 st.write(
-    """This section shows the final dataset after applying Normalization, PCA and clustering."""
+    """This section shows the final dataset after applying Normalization, PCA and K-Means clustering."""
 )
-
 
 # @st.cache_data
 def get_dataset():
     clustered_data = perform_clusterization()
-    grouped_cluster = clustered_data.groupby("Cluster")["Cluster"].count()
-    # grouped_cluster.insert(loc=0, column="Cluster", value=grouped_cluster.index)
+
+    grouped_cluster_info = clustered_data.groupby("Cluster")["Cluster"].count()
+    grouped_cluster = pd.DataFrame({
+        "Cluster Number": grouped_cluster_info.index,
+        "Number of Data Points Under Cluster": grouped_cluster_info.values
+    })
+
     return clustered_data, grouped_cluster
 
 
